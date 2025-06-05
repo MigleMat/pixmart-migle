f8490d6c05b842792facfecfa69da6d268f111e1
MigleMat <miglanda.m@gmail.com>
2025-06-05 09:31:10 +0300

    feat: apply responsive layout to catalog page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,6 +11,33 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const filterBtn = document.querySelector(
+    ".sorting-controls__filter-button-action"
+  );
+  const catalogSidebar = document.querySelector(".catalog-filters");
+  const body = document.body;
+  const closeBtn = document.querySelector(
+    ".catalog-filters__header-button-action"
+  );
+
+  if (filterBtn && catalogSidebar) {
+    filterBtn.addEventListener("click", () => {
+      const isShown = catalogSidebar.classList.contains("is-shown");
+      filterBtn.setAttribute("aria-expanded", String(!isShown));
+      catalogSidebar.classList.toggle("is-shown", !isShown);
+      body.classList.toggle("not-scrolled", !isShown); // optional scroll-lock
+    });
+  }
+  if (closeBtn) {
+    closeBtn.addEventListener("click", () => {
+      catalogSidebar.classList.remove("is-shown");
+      body.classList.remove("not-scrolled");
+      filterBtn.setAttribute("aria-expanded", "false");
+    });
+  }
+});
+
 // document.addEventListener("DOMContentLoaded", function () {
 //   // === 1. Pixmart Mobile Menu Toggle ===
 //   const burgerBtn = document.querySelector(".header__burger-menu");
